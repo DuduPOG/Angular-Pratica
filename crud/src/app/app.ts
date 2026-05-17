7c80b10fc71e45d904e8e62580e173125c7b1584
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-05-17 14:04:43 -0300

    feat: alterando a aplicação para usar os signal forms

--- crud/src/app/app.ts
+++ crud/src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import {form, FormField, minLength, required} from '@angular/forms/signals';
 import { CommonModule } from '@angular/common';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
@@ -12,27 +13,28 @@ import { CheckboxModule, Checkbox } from 'primeng/checkbox';
 interface ItemCrud {
   nome: string;
   descricao: string;
+  imagemPadrao: string;
   foto: string;
   trabalho: boolean;
   nota: string;
 }
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, CommonModule, FloatLabelModule, InputTextModule, ButtonModule, CardModule, Checkbox],
+  imports: [RouterOutlet, FormsModule, CommonModule, FloatLabelModule, InputTextModule, ButtonModule, CardModule, Checkbox, FormField],
   template: ` <main class="text-center m-auto">
     <h1 class="text-center"> {{ titulo }} </h1>
 
     <div class="card flex flex-wrap justify-center items-end gap-4 mt-4">
 
             <p-floatlabel variant="on">
-                <input #nomeInput id="nomeInput" pInputText class="on_label" [(ngModel)]="name" autocomplete="off" />
+                <input #nomeInput id="nomeInput" pInputText class="on_label" [formField]="crudForm.nome" autocomplete="off" />
                 <label for="on_label">Nome</label>
             </p-floatlabel>
     </div>
     <div class="card flex flex-wrap justify-center items-end gap-4 mt-4">
 
             <p-floatlabel variant="on">
-                <input pInputText class="on_label" [(ngModel)]="textoescrito" autocomplete="off" />
+                <input pInputText class="on_label" [formField]="crudForm.descricao" autocomplete="off" />
                 <label for="on_label">Descrição</label>
             </p-floatlabel>
     </div>
@@ -40,20 +42,20 @@ interface ItemCrud {
     <div class="card flex flex-wrap justify-center items-end gap-4 mt-4">
 
             <p-floatlabel variant="on">
-                <input pInputText class="on_label" [(ngModel)]="foto" autocomplete="off" />
+                <input pInputText class="on_label" [formField]="crudForm.foto" autocomplete="off" />
                 <label for="on_label">Foto qualquer</label>
             </p-floatlabel>
     </div>
     <div class="card flex flex-wrap justify-center items-end gap-4 mt-4">
       <div class="card flex justify-center gap-4">
         <p>Foi trabalhoso fazer esse crud? </p>
-        <p-checkbox [(ngModel)]="trabalho" [binary]="true" />
+        <p-checkbox [formField]="crudForm.trabalho" [binary]="true" />
       </div>
   </div>
   <div class="card flex flex-wrap justify-center items-end gap-4 mt-4">
 
             <p-floatlabel variant="on">
-                <input pInputText class="on_label" [(ngModel)]="nota" autocomplete="off" />
+                <input pInputText class="on_label" [formField]="crudForm.nota" autocomplete="off" />
                 <label for="on_label">Nota para seu crud</label>
             </p-floatlabel>
     </div>
@@ -127,29 +129,33 @@ interface ItemCrud {
 export class App {
   titulo = 'Projeto de CRUD';
 
-  name = '';
-  textoescrito = '';
-  foto = '';
-  imagemPadrao =
-  'https://static.wikia.nocookie.net/naruto/images/4/43/Mangeky%C3%B4_Sharingan_Shisui.svg/revision/latest?cb=20140503184904&path-prefix=fr';
-  trabalho = false;
-  nota = '0';
+  crudModel = signal<ItemCrud>({
+    nome: '',
+    descricao: '',
+    imagemPadrao: 'https://static.wikia.nocookie.net/naruto/images/4/43/Mangeky%C3%B4_Sharingan_Shisui.svg/revision/latest?cb=20140503184904&path-prefix=fr',
+    foto: '',
+    trabalho: false,
+    nota: '0'
+  });
+
+  crudForm = form(this.crudModel);
 
   editandoIndex: number | null = null;
 
   listaObjetos: ItemCrud[] = [];
 
   handleclick() {
-    if (!this.name.trim() || !this.textoescrito.trim()) {
+    if (!this.crudForm.nome().value().trim() || !this.crudForm.descricao().value().trim()) {
       return;
     }
 
     const objeto: ItemCrud = {
-      nome: this.name,
-      descricao: this.textoescrito,
-      foto: this.foto || this.imagemPadrao,
-      trabalho: this.trabalho,
-      nota: this.nota
+      nome: this.crudForm.nome().value(),
+      descricao: this.crudForm.descricao().value(),
+      imagemPadrao: 'https://static.wikia.nocookie.net/naruto/images/4/43/Mangeky%C3%B4_Sharingan_Shisui.svg/revision/latest?cb=20140503184904&path-prefix=fr',
+      foto: this.crudForm.foto().value() || this.crudForm.imagemPadrao().value(),
+      trabalho: this.crudForm.trabalho().value(),
+      nota: this.crudForm.nota().value()
     };
 
     if (this.editandoIndex !== null) {
@@ -164,12 +170,14 @@ export class App {
 
   editar(index: number) {
     const item = this.listaObjetos[index];
-
-    this.name = item.nome;
-    this.textoescrito = item.descricao;
-    this.foto = item.foto;
-    this.trabalho = item.trabalho;
-    this.nota = item.nota;
+    this.crudModel.set({
+      nome: item.nome,
+      descricao: item.descricao,
+      imagemPadrao: item.imagemPadrao,
+      foto: item.foto,
+      trabalho: item.trabalho,
+      nota: item.nota
+    });
 
     this.editandoIndex = index;
 
@@ -181,11 +189,14 @@ export class App {
   }
 
   resetarInputs() {
-    this.name = '';
-    this.textoescrito = '';
-    this.foto = '';
-    this.trabalho = false;
-    this.nota = '0';
+    this.crudModel.set({
+      nome: '',
+      descricao: '',
+      imagemPadrao: 'https://static.wikia.nocookie.net/naruto/images/4/43/Mangeky%C3%B4_Sharingan_Shisui.svg/revision/latest?cb=20140503184904&path-prefix=fr',
+      foto: '',
+      trabalho: false,
+      nota: ''
+    });
   }
 
   excluir(index: number) {
