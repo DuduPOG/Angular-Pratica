d017769b19db09c7f24b95c9a6bf290467d6293b
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-05-19 17:24:18 +0000

    feat: separando o formulário de crud em um componente menor

--- crud/src/app/app.ts
+++ crud/src/app/app.ts
@@ -1,16 +1,16 @@
-import { Component, ViewChild, ElementRef, signal, computed } from '@angular/core';
+import { Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import {form, FormField, minLength, required} from '@angular/forms/signals';
+import {form, FormField, required} from '@angular/forms/signals';
 import { CommonModule } from '@angular/common';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-import { CheckboxModule, Checkbox } from 'primeng/checkbox';
+import { Checkbox } from 'primeng/checkbox';
+import { CrudForm } from './crud-form/crud-form';
 
-
-interface ItemCrud {
+export interface ItemCrud {
   nome: string;
   descricao: string;
   imagemPadrao: string;
@@ -20,76 +20,16 @@ interface ItemCrud {
 }
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule, CommonModule, FloatLabelModule, InputTextModule, ButtonModule, CardModule, Checkbox, FormField],
+  imports: [RouterOutlet, FormsModule, CommonModule, FloatLabelModule, InputTextModule, ButtonModule, CardModule, Checkbox, FormField, CrudForm],
   template: ` <main class="text-center m-auto">
     <h1 class="text-center"> {{ titulo }} </h1>
 
-    <form (submit)="handleclick($event)">
-    <div class="card flex flex-wrap justify-center items-end gap-4 mt-4">
-
-            <p-floatlabel variant="on">
-                <input #nomeInput id="nomeInput" type="InputTextModule" pInputText class="on_label" [formField]="crudForm.nome" autocomplete="off" />
-                <label for="on_label">Nome</label>
-                    @if (crudForm.nome().invalid()) {
-                      <p class="error">O campo "nome" tem erros de validação:</p>
-                      <ul>
-                        @for (error of crudForm.nome().errors(); track error) {
-                          <li>{{ error.message }}</li>
-                        }
-                      </ul>
-                    }
-            </p-floatlabel>
-    </div>
-    <div class="card flex flex-wrap justify-center items-end gap-4 mt-4">
-
-            <p-floatlabel variant="on">
-                <input pInputText class="on_label" [formField]="crudForm.descricao" autocomplete="off" />
-                <label for="on_label">Descrição</label>
-                @if (crudForm.descricao().invalid()) {
-                  <p class="error">O campo "descrição" tem erros de validação:</p>
-                  <ul>
-                    @for (error of crudForm.nome().errors(); track error) {
-                      <li>{{ error.message }}</li>
-                    }
-                  </ul>
-                }
-            </p-floatlabel>
-    </div>
-
-    <div class="card flex flex-wrap justify-center items-end gap-4 mt-4">
-
-            <p-floatlabel variant="on">
-                <input pInputText class="on_label" [formField]="crudForm.foto" autocomplete="off" />
-                <label for="on_label">Foto qualquer</label>
-            </p-floatlabel>
-    </div>
-    <div class="card flex flex-wrap justify-center items-end gap-4 mt-4">
-      <div class="card flex justify-center gap-4">
-        <p>Foi trabalhoso fazer esse crud? </p>
-        <p-checkbox [formField]="crudForm.trabalho" [binary]="true" />
-      </div>
-  </div>
-  <div class="card flex flex-wrap justify-center items-end gap-4 mt-4">
-
-            <p-floatlabel variant="on">
-                <input pInputText class="on_label" [formField]="crudForm.nota" autocomplete="off" />
-                <label for="on_label">Nota para seu crud</label>
-                @if (crudForm.nota().invalid()) {
-                  <p class="error">O campo "nota" tem erros de validação:</p>
-                  <ul>
-                    @for (error of crudForm.nota().errors(); track error) {
-                      <li>{{ error.message }}</li>
-                    }
-                  </ul>
-                }
-            </p-floatlabel>
-    </div>
+  <form (submit)="handleclick($event)">
+    <app-crud-form [crudForm]="crudForm" ></app-crud-form>
     <div class="card flex justify-center mt-4">
       <p-button type="submit" [disabled]="crudForm().invalid()" label="Salvar" (onClick)="handleclick($event)" />
     </div>
-    </form>
-
-
+  </form>
     <div class="flex flex-wrap justify-center gap-6 mt-8">
       <p-card
         *ngFor="let item of listaObjetos; let i = index"
