795605c1d20d30993660b686b2f638e9e479505b
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-07-12 14:42:27 -0300

    fix: atualizando uris  de teste

--- frontend/crud/src/environments/environment.codespace.ts
+++ frontend/crud/src/environments/environment.codespace.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production:false,
-  apiUrl:'https://SEU_CODESPACE-8000.app.github.dev/api'
+  production: false,
+  apiUrl: 'https://zany-space-couscous-pj99g5gww65wf9p9q-8000.app.github.dev/api'
 };
